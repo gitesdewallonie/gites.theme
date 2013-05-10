9b67e86f9b9bc91f2c005c642b82a71232ead8de
Laurent Lasudry <lasudry@gmail.com>
2013-05-10 16:31:25 +0200

    Improve video splash image (was not working in Chrome)

--- gites/theme/static/scripts/gites.js
+++ gites/theme/static/scripts/gites.js
@@ -5,4 +5,10 @@ jQuery(document).ready(function($) {
       , pause: 'hover'
     })  
 
+    $('#splashvideo img').click(function(){
+        var videoUrl = $(this).attr('data-video');
+        var frame = '<iframe width="100%" height="539" src="' + videoUrl + '?rel=0&autoplay=1" frameborder="0" allowfullscreen></iframe>';
+        $(this).replaceWith(frame);
+    });
+
 });
